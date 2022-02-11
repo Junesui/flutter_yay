23659739144f04ede7dd488c835943f1b6576260
June <suizhen96@gmail.com>
2022-02-11 19:12:13 +0900

    通话页

--- lib/page/calling/calling_page.dart
+++ lib/page/calling/calling_page.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:imitate_yay/constant/common_constant.dart';
 import 'package:imitate_yay/util/screen_util.dart';
+import 'package:imitate_yay/widget/my_cache_net_img.dart';
+import 'package:imitate_yay/widget/my_icon_btn.dart';
 import 'package:imitate_yay/widget/my_text.dart';
 import 'package:imitate_yay/widget/my_upcount.dart';
+import 'package:lottie/lottie.dart';
 
 /// 通话页面
 class CallingPage extends StatefulWidget {
@@ -18,7 +21,6 @@ class _CallingPageState extends State<CallingPage> {
   final FocusNode _inputFocusNode = FocusNode();
   //输入框控制器
   final TextEditingController _editingController = TextEditingController();
-
   // 输入框后面的图标是否是拷贝
   bool _isCopy = true;
 
@@ -53,6 +55,12 @@ class _CallingPageState extends State<CallingPage> {
               _buildAppBar(),
               const SizedBox(height: 6),
               _buildInput(),
+              Expanded(child: _buildUserAvatars()),
+              const MyText(text: "他の参加者を待っています...", fontSize: 50),
+              const SizedBox(height: 6),
+              _buildSmallPlayIcon(),
+              const SizedBox(height: 6),
+              _buildBigFuncionIcon(),
             ],
           ),
         ),
@@ -106,13 +114,16 @@ class _CallingPageState extends State<CallingPage> {
         ),
         const SizedBox(width: 3),
         // 最小化按钮
-        Chip(
-          backgroundColor: Colors.grey[800],
-          shape: const CircleBorder(),
-          label: Icon(
-            Icons.expand_more,
-            color: Colors.white70,
-            size: SU.setFontSize(65),
+        GestureDetector(
+          onTap: () {},
+          child: Chip(
+            backgroundColor: Colors.grey[800],
+            shape: const CircleBorder(),
+            label: Icon(
+              Icons.expand_more,
+              color: Colors.white70,
+              size: SU.setFontSize(65),
+            ),
           ),
         ),
       ],
@@ -174,6 +185,133 @@ class _CallingPageState extends State<CallingPage> {
             size: SU.setFontSize(62),
           ),
         ),
+        suffix: const MyText(
+          text: "・by suiz",
+          fontSize: 32,
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+
+  /// 中间用户头像区域
+  _buildUserAvatars() {
+    String testAvatarUrl =
+        "https://images.pexels.com/photos/4386364/pexels-photo-4386364.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260";
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Lottie.asset("assets/animation/waitting_ripple.json"),
+        SizedBox(
+          height: SU.setHeight(300),
+          width: SU.setHeight(300),
+          child: Stack(
+            children: [
+              // 头像
+              CircleAvatar(
+                radius: SU.setHeight(150),
+                backgroundColor: Colors.transparent,
+                backgroundImage: MyCacheNetImg.provider(testAvatarUrl),
+              ),
+              // 房主图标
+              Positioned(
+                left: SU.setWidth(20),
+                child: CircleAvatar(
+                  radius: SU.setHeight(32),
+                  backgroundColor: CommonConstant.primaryColor,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                    size: SU.setFontSize(46),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// 四个小按钮 【游戏，表情，说话，设置】
+  _buildSmallPlayIcon() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildSmallPlayIconItem(Icons.sports_esports),
+        _buildSmallPlayIconItem(Icons.tag_faces),
+        _buildSmallPlayIconItem(Icons.record_voice_over),
+        _buildSmallPlayIconItem(Icons.settings),
+      ],
+    );
+  }
+
+  /// 小按钮子项
+  _buildSmallPlayIconItem(IconData icon) {
+    return Chip(
+      backgroundColor: Colors.grey[800],
+      shape: const CircleBorder(),
+      padding: const EdgeInsets.all(5),
+      label: Icon(icon, size: SU.setFontSize(62), color: Colors.white70),
+    );
+  }
+
+  /// 最底部四个大按钮 【音乐，扩声器，麦克风，退出】
+  _buildBigFuncionIcon() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(SU.setHeight(36)),
+        color: Colors.grey[800],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildBigFuncionIconItem(Icons.music_note, Colors.white70, "BGM", true, () {}),
+          _buildVerticalDiveder(),
+          _buildBigFuncionIconItem(Icons.volume_up, Colors.white70, "スピーカー", true, () {}),
+          _buildVerticalDiveder(),
+          _buildBigFuncionIconItem(Icons.mic, Colors.white70, "マイク", true, () {}),
+          _buildVerticalDiveder(),
+          _buildBigFuncionIconItem(Icons.logout, Colors.red, "退出", false, () {
+            Navigator.of(context).pop();
+          }),
+        ],
+      ),
+    );
+  }
+
+  /// 最底部四个大按钮子项
+  _buildBigFuncionIconItem(
+      IconData icon, Color color, String text, bool isHasRightLine, VoidCallback ontap) {
+    return Row(
+      children: [
+        Column(
+          children: [
+            MyIconBtn(
+              onPressed: ontap,
+              icon: icon,
+              size: 70,
+              color: color,
+            ),
+            const SizedBox(height: 3),
+            MyText(
+              text: text,
+              color: color,
+              fontSize: 30,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  /// 垂直分割线
+  _buildVerticalDiveder() {
+    return SizedBox(
+      height: SU.setHeight(100),
+      child: const VerticalDivider(
+        color: Colors.white12,
       ),
     );
   }
